b2b853298deb8fa206dad59bfe03bc65e1d227f8
Jamil Bashir <jamil@themindgauge.com>
2020-07-18 15:31:20 +0500

    added aragon plugin

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# wp-paragon-connect
+# Basic React Files for WordPress
+The repository contains the very basic Required files to create a react app with JSX (in WordPress).
--- aragon-connect-plugin.php
+++ aragon-connect-plugin.php
@@ -7,13 +7,57 @@
  * Author: Jamil Bashir
  * Author URI: 
  */
+/**
+ * WordPress Setting Page for a React.
+ *
+ * @package WordPress
+ * @since 1.0.0
+ */
 
-import { connect } from '@aragon/connect'
+//_____________________Combining ReactJS & WordPress
 
-// Initiates the connection to an organization
-const org = await connect('example.aragonid.eth', 'thegraph')
 
-// Fetch the apps belonging to this organization
-const apps = await org.apps()
+// Action to add menu in settings page.
+add_action( 'admin_menu', 'wpreact_admin_menu' );
 
-apps.forEach(console.log)
+/**
+ * Function to add new menu in settings
+ *
+ * @since   1.0.0
+ */
+function wpreact_admin_menu() {
+	add_options_page(
+		__( 'React Page', 'wpreact' ),
+		__( 'React Page', 'wpreact' ),
+		'manage_options',
+		'wpreact-page',
+		'display_wpreact_admin_page'
+	);
+}
+
+/**
+ * Callback function of Setting Page
+ *
+ * @since    1.0.0
+ */
+function display_wpreact_admin_page() {
+	?>
+	<h1>Welcome..!</h1><br />
+        <div id="root" style="border: 1px solid red; padding : 25px; width: 800px;">This is 'root' div.</div>
+	<?php
+}
+
+// Action to add scripts to admin side.
+add_action( 'admin_enqueue_scripts', 'wpreact_enqueue_admin_scripts' );
+
+/**
+ * Function to add the scripts and styles to admin page.
+ *
+ * @since 1.0.0
+ */
+function wpreact_enqueue_admin_scripts() {
+	wp_enqueue_script( 'wpreact-script', get_stylesheet_directory_uri() . '/dist/bundle.js', array(
+		'jquery',
+		'wp-element'
+	), '1.0.1' );
+}
